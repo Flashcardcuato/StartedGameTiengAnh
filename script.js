a4a650b214bc34bf4a2bbebd8d34cfd1b9a15458
WyAsta <huynhgiahuys3883@gmail.com>
2026-09-22 21:18:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function initAudio() {
     audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
     masterGain = audioContext.createGain();
-    masterGain.gain.value = 3;
+    masterGain.gain.value = 2;
 
     masterGain.connect(audioContext.destination);
 
